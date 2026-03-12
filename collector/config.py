932a1430ef4329b51a9afd05874b0279ec16d0a5
codertz26-code <codertz26@gmail.com>
2026-03-12 14:17:40 +0300

    SILA

--- collector/config.py
+++
@@ -1,3 +0,0 @@
-# Mipangilio ya mfumo
-INTERVAL = 60  # Sekunde ngapi kabla ya kukusanya data tena
-DB_PATH = "/sdcard/Download/data.db"  # Mahali pa kuweka database (unaweza kuiona kwenye Downloads)
